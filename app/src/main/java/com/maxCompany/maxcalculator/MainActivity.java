eca3de403d54f06dc289456321a2b9e7ad593ee2
maxambrosii <maxambrosii@mail.ru>
2023-10-24 18:50:13 +0300

    did all implementation

---
+++ app/src/main/java/com/maxCompany/maxcalculator/MainActivity.java
@@ -0,0 +1,151 @@
+package com.maxCompany.maxcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView resultField; // текстовое поле для вывода результата
+    EditText numberField;   // поле для ввода числа
+    TextView operationField; // текстовое поле для вывода знака операции
+    Double operand = null;  // операнд операции
+    String lastOperation = "="; // последняя операция
+    Switch colorSwitch; // Добавьте Switch
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // получаем все поля по id из activity_main.xml
+        resultField = findViewById(R.id.resultField);
+        numberField = findViewById(R.id.numberField);
+        operationField = findViewById(R.id.operationField);
+        colorSwitch = findViewById(R.id.colorSwitch);
+        // Назначьте обработчик для Switch
+        colorSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                changeButtonColor(isChecked);
+            }
+        });
+    }
+    private void changeButtonColor(boolean isRed) {
+        // Изменить цвет всех кнопок в соответствии с параметром isRed
+        int color;
+        if (isRed) {
+            color = Color.BLUE;
+        } else {
+            color = Color.parseColor("#9400D3");
+        }
+
+        // Найдите все кнопки и измените их цвет
+        int[] buttonIds = new int[]{
+                R.id.firstButtonPanel,
+                R.id.secondButtonPanel,
+                R.id.thirdButtonPanel,
+                R.id.forthButtonPanel,
+        };
+
+        for (int buttonId : buttonIds) {
+            LinearLayout button = findViewById(buttonId);
+            button.getChildAt(0).setBackgroundColor(color);
+            button.getChildAt(1).setBackgroundColor(color);
+            button.getChildAt(2).setBackgroundColor(color);
+            button.getChildAt(3).setBackgroundColor(color);
+            //button.setBackgroundColor(color);
+        }
+    }
+    // сохранение состояния
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if(operand!=null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+    // получение ранее сохраненного состояния
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        lastOperation = savedInstanceState.getString("OPERATION");
+        operand= savedInstanceState.getDouble("OPERAND");
+        resultField.setText(operand.toString());
+        operationField.setText(lastOperation);
+    }
+    // обработка нажатия на числовую кнопку
+    public void onNumberClick(View view){
+
+        Button button = (Button)view;
+        numberField.append(button.getText());
+
+        if(lastOperation.equals("=") && operand!=null){
+            operand = null;
+        }
+    }
+    // обработка нажатия на кнопку операции
+    public void onOperationClick(View view){
+
+        Button button = (Button)view;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+        // если введенно что-нибудь
+        if(number.length()>0){
+            number = number.replace(',', '.');
+            try{
+                performOperation(Double.valueOf(number), op);
+            }catch (NumberFormatException ex){
+                numberField.setText("");
+            }
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+
+    private void performOperation(Double number, String operation){
+
+        // если операнд ранее не был установлен (при вводе самой первой операции)
+        if(operand ==null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand =0.0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "*":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace('.', ','));
+        numberField.setText("");
+    }
+}
